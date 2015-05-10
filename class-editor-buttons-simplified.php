e09eef9a56530ce51ee134017a925caffaea9132
Aki Björklund <aki@h1.fi>
2015-05-10 13:54:10 +0300

    unprefix class functions

--- class-editor-buttons-simplified.php
+++ class-editor-buttons-simplified.php
@@ -1,31 +1,39 @@
 <?php
-
 /*
-	Copyright 2015 Aki Björklund
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 3, as
-	published by the Free Software Foundation.
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+Copyright 2015 Aki Björklund
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 3, as
+published by the Free Software Foundation.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 */
 
+/**
+ * TinyMCE filters for better editor functionality.
+ */
 class Editor_Buttons_Simplified {
 
+	/**
+	 * Hook to TinyMCE filters
+	 */
 	function __construct() {
-		add_filter( 'mce_buttons',          array( $this, 'ebs_tinymce_buttons_row_1' ) );
-		add_filter( 'mce_buttons_2',        array( $this, 'ebs_tinymce_buttons_row_2' ) );
-		add_filter( 'tiny_mce_before_init', array( $this, 'ebs_tinymce_block_format_mod' ) );
+		add_filter( 'mce_buttons',          array( $this, 'tinymce_buttons_row_1' ) );
+		add_filter( 'mce_buttons_2',        array( $this, 'tinymce_buttons_row_2' ) );
+		add_filter( 'tiny_mce_before_init', array( $this, 'tinymce_block_format_mod' ) );
 	}
 
 	/**
 	 * Remove useless alignment buttons, add formatselect to row 1
 	 */
-	function ebs_tinymce_buttons_row_1( $buttons ) {
+	function tinymce_buttons_row_1( $buttons ) {
 		$remove = array( 'strikethrough', 'hr', 'alignleft', 'aligncenter', 'alignright' );
 		return array_merge( array( 'formatselect' ), array_diff( $buttons, $remove ) );
 	}
@@ -33,7 +41,7 @@ class Editor_Buttons_Simplified {
 	/**
 	 * Remove more useless buttons
 	 */
-	function ebs_tinymce_buttons_row_2( $buttons ) {
+	function tinymce_buttons_row_2( $buttons ) {
 		$remove = array( 'formatselect', 'underline', 'alignjustify', 'forecolor', 'outdent', 'indent' );
 		return array_merge( array( 'strikethrough', 'hr' ), array_diff( $buttons, $remove ) );
 	}
@@ -41,7 +49,7 @@ class Editor_Buttons_Simplified {
 	/**
 	 * Remove useless heading levels, move pre to bottom.
 	 */
-	function ebs_tinymce_block_format_mod( $settings ) {
+	function tinymce_block_format_mod( $settings ) {
 		$settings['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4';
 		return $settings;
 	}
--- editor-buttons-simplified.php
+++ editor-buttons-simplified.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: Editor Buttons Simplified
 Plugin URI: http://akibjorklund.com/2015/editor-buttons-simplified
@@ -10,21 +9,24 @@ Author URI: http://akibjorklund.com
 */
 
 /*
-	Copyright 2015 Aki Björklund
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 3, as
-	published by the Free Software Foundation.
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+Copyright 2015 Aki Björklund
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 3, as
+published by the Free Software Foundation.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 */
 
-require_once( 'class-editor-buttons-simplified.php' );
+require_once 'class-editor-buttons-simplified.php';
 
-add_action( 'admin_init', function() {
+add_action( 'admin_init', function () {
 	new Editor_Buttons_Simplified;
 } );
