cd5400596b03a3c725acfc7168eb29730cb75581
Aki Björklund <aki@h1.fi>
2015-05-11 11:06:57 +0300

    store strings in the settings

--- assets/js/ebs_more_plugin.js
+++ assets/js/ebs_more_plugin.js
@@ -5,17 +5,17 @@
 		// Get base url of assets.
 		var assets_url = url.replace( 'assets/js', 'assets' );
 
-		// Show or hide extra buttons. Show = 1 to show, 0, to hide.
+		// Show or hide extra buttons. Show = '1' to show, '0', to hide.
 		function showHide( show ) {
 			$('.mce-wp-dfw').nextAll( ':not(#ebs_more)' ).each( function () {
 				var that = $(this);
-				0 === show ? that.hide() : that.css( 'display', 'inline-block' );
+				'0' === show ? that.hide() : that.css( 'display', 'inline-block' );
 			} );
 		}
 
 		// Define the command that executes when the more button is clicked.
 		editor.addCommand( 'ebs_expand', function() {
-			var setting = $( '#ebs_more' ).prev().is( ':visible' ) ? 0 : 1;
+			var setting = $( '#ebs_more' ).prev().is( ':visible' ) ? '0' : '1';
 			setUserSetting( 'ebs_show_more', setting );
 			showHide( setting );
 		} );
@@ -23,10 +23,9 @@
 		// On init, toggle buttons by setting.
 		editor.on( 'init', function( editor ) {
 			var setting = getUserSetting( 'ebs_show_more' );
-			console.log(setting);
 			// Default to not showing everything.
-			if ( null === setting ) {
-				setting = 0;
+			if ( '' === setting ) {
+				setting = '0';
 			}
 			showHide( setting );
 		} );
